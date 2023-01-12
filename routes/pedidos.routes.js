24347642a4b6580bf1f8bed6b48a6c78992043e0
Karlos-Daniel <68357442+Karlos-Daniel@users.noreply.github.com>
2023-01-11 22:21:06 -0500

    validar id´s en las rutas pedidos
    validar id´s en las rutas pedidos

--- routes/pedidos.routes.js
+++ routes/pedidos.routes.js
@@ -10,24 +10,38 @@ router.post('/crearPedido',crearPedido);//LISTO
 router.get('/obtenerPedidos',obtenerPedidos);//LISTO
 router.get('/obtenerVentas',obtenerVentas);//LISTO
 
-router.get('/productos/pedido/:id',obtenerProductosPedido);//LISTO
+router.get('/productos/pedido/:id',[
+    check('id','No es un ID valido de MongoDB').isMongoId(),
+],obtenerProductosPedido);//LISTO
 
 //esto va a editar producto en detallesVenta
-router.put('/editar/ProductoPedido/:idProducto/:idVenta',editarProductoPedido);//LISTO
+router.put('/editar/ProductoPedido/:idProducto/:idVenta',[
+    check('idProducto','No es un ID valido de MongoDB').isMongoId(),
+    check('idVenta','No es un ID valido de MongoDB').isMongoId(),
+],editarProductoPedido);//LISTO
 
 //agrega producto a una venta
-router.post('/agregarProducto/:id',agregarProducto)//LISTO
+router.post('/agregarProducto/:id',[
+    check('id','No es un ID valido de MongoDB').isMongoId(),
+],agregarProducto)//LISTO
 
 //esto va a borrar producto en detallesVenta
 //ID tiene que ser de detalles venta
 //middleware para ver si existe venta
-router.delete('/borrar/productoPedido/:idProducto/:idVenta',borrarProductoPedido);//LISTO
+router.delete('/borrar/productoPedido/:idProducto/:idVenta',[
+    check('idProducto','No es un ID valido de MongoDB').isMongoId(),
+    check('idVenta','No es un ID valido de MongoDB').isMongoId(),
+],borrarProductoPedido);//LISTO
 
 //esto va a editar el valor de Ventas
-router.put('/editar/venta/:id',editarVenta);//LISTO
+router.put('/editar/venta/:id',[
+    check('id','No es un ID valido de MongoDB').isMongoId(),
+],editarVenta);//LISTO
 
 //esto va a borrar la venta
-router.delete('/borrar/venta/:id',borrarVenta);//LISTO
+router.delete('/borrar/venta/:id',[
+    check('id','No es un ID valido de MongoDB').isMongoId(),
+],borrarVenta);//LISTO
 
 
 module.exports = router;
